6335469484b1d52520794032f353782792543886
Bernard Wijaya <35998190+MasayukiBernard@users.noreply.github.com>
2019-05-15 21:02:29 +0700

    Premimum Account Scrolling Animation
    with css edits

--- js/premium-acc.js
+++ js/premium-acc.js
@@ -1,25 +1,32 @@
-function scrollPageUpwards(){
-    var pixels = 2140;
-    while(pixels >= 0)
-    {
-        window.scrollBy(0, pixels)
-        pixels = pixels - 1;
-    }
-}
-
-function directScrollDownwards(){
-    window.scrollBy(0, 2140);
-}
-
 var timeOut;
-function scrollToTop() {
-    document.onscroll = return;
-	if (document.body.scrollTop!=2140 || document.documentElement.scrollTop!=2140){
-		window.scrollBy(0,2);
-		timeOut=setTimeout('scrollToTop()',1);
+// Initial scrolling state
+var scrollPos = 0;
+var scroll = true;
+
+// Adding the scrol event
+window.addEventListener('scroll', function(){
+    // detects new state and compares it with the new one
+    if ((document.body.getBoundingClientRect()).top > scrollPos)
+          scroll = false;
+      // saves the new position for iteration.
+      scrollPos = (document.body.getBoundingClientRect()).top;
+  });
+
+function scrollToBot() {
+    if(scroll == false){
+        return;
     }
-	else clearTimeout(timeOut);
+    
+    var x = document.body.scrollTop;
+    var y = document.documentElement.scrollTop;
+
+	if (x !=2140 || y!=2140){
+		window.scrollBy(0,2);
+		timeOut=setTimeout('scrollToBot()',1);
+    }
+    else clearTimeout(timeOut);
 }
 
-// directScrollDownwards();
-scrollToTop();
+scrollToBot();
+
+
